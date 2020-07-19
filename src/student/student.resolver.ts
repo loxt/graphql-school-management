dc03ead0b02bb73c338fcd714781b7166b5ea3f9
loxt <emannuelloxt@hotmail.com>
2020-07-19 17:53:45 -0400

    :sparkles: feat(student): create get-all-students method

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { StudentType } from './student.type';
 import { CreateStudentInput } from './create-student.input';
 import { StudentService } from './student.service';
@@ -6,6 +6,12 @@ import { StudentService } from './student.service';
 @Resolver(of => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query(returns => [StudentType])
+  async students() {
+    return this.studentService.getStudents();
+  }
+
   @Mutation(returns => StudentType)
   async createStudent(
     @Args('createStudentInput') createStudentInput: CreateStudentInput,
